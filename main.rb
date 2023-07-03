74ca28b02648e8e90d684a354b6442e8c0e44bd6
Naji-97 <elbairabennaji@gmail.com>
2023-07-03 17:30:28 +0100

    refactor main.rb to solve the linter error

--- main.rb
+++ main.rb
@@ -1,45 +1,48 @@
+def display_menu
+  puts 'School Library System'
+  puts '1. List all books'
+  puts '2. List all labels'
+  puts '3. List all music albums'
+  puts '4. List all genres'
+  puts '5. List all games'
+  puts '6. List all authors'
+  puts '7. Add a book'
+  puts '8. Add a music album'
+  puts '9. Add a game'
+  puts '10. Exit'
+  puts 'Enter your choice (1-10):'
+end
+
+def process_choice(choice)
+  actions = {
+    1 => 'All books',
+    2 => 'All labels',
+    3 => 'All music albums',
+    4 => 'All genres',
+    5 => 'All games',
+    6 => 'All authors',
+    7 => 'Add a book',
+    8 => 'Add a music album',
+    9 => 'Add a game',
+    10 => 'Thank you for using this app. See you around soon!'
+  }
+
+  if actions.key?(choice)
+    puts actions[choice]
+    return choice == 10
+  end
+
+  puts 'Invalid choice. Please enter a number from 1 to 10.'
+  false
+end
+
 def main
   loop do
-    puts 'School Library System'
-    puts '1. List all books'
-    puts '2. List all labels'
-    puts '3. List all music albums'
-    puts '4. List all genres'
-    puts '5. List all games'
-    puts '6. List all authors'
-    puts '7. Add a book'
-    puts '8. Add a music album'
-    puts '9. Add a game'
-    puts '10. Exit'
-    puts 'Enter your choice (1-4):'
+    display_menu
 
     choice = gets.chomp.to_i
 
-    case choice
-    when 1
-      puts 'All books'
-    when 2
-      puts 'All books7'
-    when 3
-      puts 'Add new movie'
-    when 4
-      puts 'Add new Genre'
-    when 5
-      puts 'Add new '
-    when 6
-      puts ' new book'
-    when 7
-      puts 'Add book'
-    when 8
-      puts 'Add new book5'
-    when 9
-      puts 'Add new book3'
-    when 10
-      puts 'Thank you for using this app. See you around soon!'
-      break
-    else
-      puts 'Invalid choice. Please enter a number from 1 to 7.'
-    end
+    break if process_choice(choice)
 
     puts "\n"
   end
