d23aff2b2a77be735b9dbf2a327a6af9a37ce757
eudondian <eudondian@gmail.com>
2023-07-03 17:00:34 +0100

    Fixed rubocop error

--- item.rb
+++ item.rb
@@ -1,24 +1,24 @@
 class Item
-     attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
 
-    def initialize
-        @id = Random.rand(1..1000)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def can_be_archived?
-        publication = Time.now.year - @publish_date.year
-    end
+  def can_be_archived?
+    Time.now.year - @publish_date.year
+  end
 
-    def move_to_archive
-        if can_be_archived?
-            @archived = true
-        end
-    end
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
 end
