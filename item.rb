7b083801a0ba8c49561cc1403789318d0c0d787f
Naji-97 <elbairabennaji@gmail.com>
2023-07-03 16:42:07 +0100

    add can_be_achieved_methode

--- item.rb
+++ item.rb
@@ -6,13 +6,15 @@ class Item
         @id = Random.rand(1..1000)
         @genre = genre
         @author = author
-        @source = source 
+        @source = source
         @label = label
         @publish_date = publish_date
         @archived = archived
     end
 
-    
+    def can_be_archived?
+        publication = Time.now.year - @publish_date.year
+    end
 
     def move_to_archive
         if can_be_archived?
