782df5b8198e32134e3e5985a8e122dec8edfb86
prius2055 <prius2055@gmail.com>
2023-07-05 17:39:26 +0100

    update book.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,25 +2,22 @@ require_relative '../book'
 
 
 describe "#can_be_archived?" do
-   
-      it 'returns true when move_to_archive is true' do
+    it 'returns true when move_to_archive is true' do
         book = Book.new("John Doe", "Publisher XYZ", "2022-01-01", false, "ABC Publishing", "good")
         allow(book).to receive(:move_to_archive).and_return(true)
         expect(book.send(:can_be_archived?)).to be_truthy
-      end
- 
-
-   
-      it "sets archived to true when cover_state is 'bad'" do
+    end
+    
+    it "sets archived to true when cover_state is 'bad'" do
         cover_state = "bad"
         book = Book.new("John Doe", "Publisher XYZ", "2022-01-01", true, "ABC Publishing", cover_state)
         expect(book.archived).to be_truthy
     end
-
-      it "returns false when move_to_archive is false and cover_state is not 'bad'" do
+    
+    it "returns false when move_to_archive is false and cover_state is not 'bad'" do
         book = Book.new("John Doe", "Publisher XYZ", "2022-01-01", false, "ABC Publishing", "good")
         allow(book).to receive(:move_to_archive).and_return(false)
         expect(book.send(:can_be_archived?)).to be_falsy
-      end
+    end
  
 end
