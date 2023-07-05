82f8d44171e96a147ce7b9d7251ad5acc1b8dd14
prius2055 <prius2055@gmail.com>
2023-07-05 16:10:10 +0100

    Update book test

--- book.rb
+++ book.rb
@@ -1,3 +1,20 @@
 require './item'
 
+class Book < Item
+    attr_accessor :publisher, :cover_state
+  def initialize(author, source, publish_date, archived, publisher, cover_state, label = nil, genre = 'fiction')
+    super(genre, author, source, label, publish_date, archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
+  private
+
+  def can_be_archived?
+    if move_to_archive || @cover_state == 'bad'
+      true
+    else
+      false
+    end
+  end
+end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,16 +1,2 @@
 require_relative '../book'
 
-describe Book do
-  describe "#can_be_archived?" do
-    let(:label) { Label.new('red', 'Important') }
-   let(:item) { Item.new('genre', 'author', 'source', 'label', 'publish_date', 'archived') }
-   
-
-    it "adds an item to the label" do
-      label.add_item(item)
-      expect(label.items.size).to eq(1)
-      expect(label.items.first).to eq(item)
-      expect(item.label).to eq(label)
-    end
-  end
-end
