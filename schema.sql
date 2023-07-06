54cc52b7e11c712b04dd204ed8e9f4c79f48e3f9
prius2055 <prius2055@gmail.com>
2023-07-06 16:48:46 +0100

    create table for books

--- schema.sql
+++ schema.sql
@@ -7,3 +7,15 @@ CREATE TABLE items (
   publish_date INTEGER,
   archived BOOLEAN
 );
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  genre VARCHAR(255),
+  author VARCHAR(255),
+  source VARCHAR(255),
+  label_id VARCHAR(255),
+  publish_date VARCHAR(255),
+  archived VARCHAR(255),
+  publisher VARCHAR(255),
+  cover_state VARCHAR(255),
+);
