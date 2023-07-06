019d29b7a1f16c2ae88cc3fd5ba30027103b212e
prius2055 <prius2055@gmail.com>
2023-07-06 16:29:41 +0100

    fix linters error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,13 +7,13 @@ describe '#can_be_archived?' do
     expect(book.send(:can_be_archived?)).to be_truthy
   end
 
-    it "sets archived to true when cover_state is 'bad'" do
+  it "sets archived to true when cover_state is 'bad'" do
     cover_state = 'bad'
     book = Book.new('John Doe', '2022-01-01', true, 'ABC Publishing', cover_state)
     expect(book.archived).to be_truthy
   end
 
-   it "returns false when move_to_archive is false and cover_state is not 'bad'" do
+  it "returns false when move_to_archive is false and cover_state is not 'bad'" do
     book = Book.new('John Doe', '2022-01-01', false, 'ABC Publishing', 'good')
     allow(book).to receive(:move_to_archive).and_return(false)
     expect(book.send(:can_be_archived?)).to be_falsy
