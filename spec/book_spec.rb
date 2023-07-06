8074e12a34938907a97540b89bff6321abaa144e
prius2055 <prius2055@gmail.com>
2023-07-06 16:24:12 +0100

    Set up test for can_be_archived? method using cover-state

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,10 @@ describe '#can_be_archived?' do
     allow(book).to receive(:move_to_archive).and_return(true)
     expect(book.send(:can_be_archived?)).to be_truthy
   end
+
+    it "sets archived to true when cover_state is 'bad'" do
+    cover_state = 'bad'
+    book = Book.new('John Doe', 'Publisher XYZ', '2022-01-01', true, 'ABC Publishing', cover_state)
+    expect(book.archived).to be_truthy
+  end
 end
