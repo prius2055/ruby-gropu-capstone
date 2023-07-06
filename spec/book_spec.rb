07e629c53c0a338b0c27bf4988bc7af821bf5c3c
prius2055 <prius2055@gmail.com>
2023-07-06 16:26:37 +0100

    Fix test errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,12 +9,12 @@ describe '#can_be_archived?' do
 
     it "sets archived to true when cover_state is 'bad'" do
     cover_state = 'bad'
-    book = Book.new('John Doe', 'Publisher XYZ', '2022-01-01', true, 'ABC Publishing', cover_state)
+    book = Book.new('John Doe', '2022-01-01', true, 'ABC Publishing', cover_state)
     expect(book.archived).to be_truthy
   end
 
    it "returns false when move_to_archive is false and cover_state is not 'bad'" do
-    book = Book.new('John Doe', 'Publisher XYZ', '2022-01-01', false, 'ABC Publishing', 'good')
+    book = Book.new('John Doe', '2022-01-01', false, 'ABC Publishing', 'good')
     allow(book).to receive(:move_to_archive).and_return(false)
     expect(book.send(:can_be_archived?)).to be_falsy
   end
