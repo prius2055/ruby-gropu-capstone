a56170376b4443a2acfff29c1a87b4a2487277ce
prius2055 <prius2055@gmail.com>
2023-07-06 16:49:17 +0100

    create table for labels

--- schema.sql
+++ schema.sql
@@ -19,3 +19,9 @@ CREATE TABLE books (
   publisher VARCHAR(255),
   cover_state VARCHAR(255),
 );
+
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  color VARCHAR(255),
+);
