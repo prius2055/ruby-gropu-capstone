396505c924d23430d45f97cd8c61930681b07584
eudondian <eudondian@gmail.com>
2023-07-06 10:58:27 +0100

    Updated game_methods file

--- game_methods.rb
+++ game_methods.rb
@@ -1,7 +1,41 @@
+require './game'
+require './author'
+
+
 class GameMethods
-  def add_author(author)
-    @author = author
-    author.items.push(self) unless author.items include?(self)
+  attr_accessor :games, :authors
+
+  def initialize
+    @game = []
+    @author = []
+  end
+
+  def list_authors
+    if @authors.empty?
+      puts 'No authors available.'
+    else
+      puts 'List of authors: '
+      @authors.each do |author|
+        puts "First Name: #{author.first_name}"
+        puts "Last Name: #{author.last_name}"
+        puts '-----------------'
+      end
+    end
+  end
+
+  def list_games
+    if @games.empty?
+      puts 'No games available'
+
+    else
+      puts 'List of game: '
+      @games.each do |game|
+        puts "Multiplayer: #{game.multiplayer}"
+        puts "Last played At: #{game.last_played_at}"
+        puts "Publish Date: #{game.publish_date}"
+        puts '------------------'
+      end
+    end
   end
 
   def add_game
@@ -38,5 +72,10 @@ class GameMethods
     author.add_items(game)
 
     puts "Game  '#{game.publish_date}', '#{game.multiplayer}, '#{game.last_played_at} added successfully"
+  end
+
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items include?(self)
   end
 end
